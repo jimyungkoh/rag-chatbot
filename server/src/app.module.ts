9d3894aa0e21b7db7fa5ca11c62eb3d095560e1d
jimyungkoh <kojimyung1206@naver.com>
2025-08-10 14:49:32 +0900

    refactor(server): 앱 컨트롤러/모듈 정리 및 부트스트랩(main) 개선

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ChromaModule } from './chroma/chroma.module';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { ChromaModule } from '@/chroma/chroma.module';
 
 @Module({
   imports: [ChromaModule],
